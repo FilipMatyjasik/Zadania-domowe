6dcfe526ac46aead16249c41ca491570ddbcd0ca
FilipMatyjasik <matyjasik.f@gmail.com>
2024-10-14 18:33:35 +0200

    Add files via upload

---
+++ Zad 1.4.1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+int a, b;
+cout << "Podaj 2 liczby." << endl;
+cin >> a >> b;
+cout << max( a, b << " jest największa." << endl;
+return 0;
+}
---
+++ Zad 1.4.2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+int a,b,c,m;
+cout << "Podaj 3 liczby." << endl;
+cin >>a>>b>> c;
+
+m=a;
+if (b>m) m=b;
+if (c>m) m=c;
+cout << "Największa liczba to: " << m;
+}
