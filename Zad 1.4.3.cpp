16b5faf7c4b5fffdf63d2088bbb73584ac78043f
FilipMatyjasik <matyjasik.f@gmail.com>
2024-10-25 15:30:48 +0200

    Add files via upload

---
+++ Zad 1.4.3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cout << "Podaj 3 liczby." << endl;
+	cin >> a >> b >> c;
+	int arr[] { a, b, c };
+	int max = arr[0];
+	int i;
+	for (i = 1; i < 3; i++)
+	{
+		if (max < arr[i])
+			max = arr[i];
+	}
+    cout << "Największa liczba to: " << max;
+	return 0;
+}
