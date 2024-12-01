265eeafc1bf199ce3cae6d7a6c69ac0a32c18123
FilipMatyjasik <matyjasik.f@gmail.com>
2024-12-01 14:42:37 +0100

    Update Zad 4.3.cpp

--- Zad 4.3.cpp
+++ Zad 4.3.cpp
@@ -3,17 +3,15 @@
 using namespace std;
 
 int main() {
-    // Deklaracja tablicy 10x10
-    int tablica[10][10] = { 0 }; // Wszystkie elementy sπ poczπtkowo ustawione na 0
-
-    // Wype≥nianie tablicy zgodnie z wymaganiami
+    int tablica[10][10] = { 0 };
+    // Wype≈Çnianie tablicy zgodnie z wymaganiami
     for (int i = 0; i < 10; ++i) {
         for (int j = 0; j < 10; ++j) {
-            tablica[i][0] = i;                          // W pierwszej kolumnie liczby od 0 do 9
-            tablica[i][1] = i + i;            // W drugiej kolumnie suma liczb od 0 do i
-            tablica[i][2] = i * i;                      // W trzeciej kolumnie kwadraty liczb
-            tablica[i][3] = i + 1 + j + 1;                  // W czwartej kolumnie suma numeru wiersza i kolumny
-            tablica[i][4] = i - j;                      // W piπtej kolumnie rÛønica numeru wiersza i kolumny
+            tablica[i][0] = i;
+            tablica[i][1] = i + i;
+            tablica[i][2] = i * i;
+            tablica[i][3] = i + 1 + j + 1;
+            tablica[i][4] = i - j;
             cout << tablica[i][j] << "\t";
         }
         cout << endl;
