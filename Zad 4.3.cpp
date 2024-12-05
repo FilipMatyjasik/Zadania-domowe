f34e0ceca020a3b354aca2d95473902fc9ce4888
FilipMatyjasik <matyjasik.f@gmail.com>
2024-12-05 17:23:18 +0100

    Update Zad 4.3.cpp

--- Zad 4.3.cpp
+++ Zad 4.3.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-
+#include <conio.h>
+#include <iomanip>
 using namespace std;
-
 int main() {
     int tablica[10][10] = { 0 };
-    // Wypełnianie tablicy zgodnie z wymaganiami
     for (int i = 0; i < 10; ++i) {
         for (int j = 0; j < 10; ++j) {
             tablica[i][0] = i;
@@ -12,10 +11,10 @@ int main() {
             tablica[i][2] = i * i;
             tablica[i][3] = i + 1 + j + 1;
             tablica[i][4] = i - j;
-            cout << tablica[i][j] << "\t";
+            cout << setw(4) << tablica[i][j];
         }
         cout << endl;
     }
-
+    getch();
     return 0;
 }
