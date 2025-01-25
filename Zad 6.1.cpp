cb365b4daa1d2168e164346bc85f1713c94ea6cd
FilipMatyjasik <matyjasik.f@gmail.com>
2025-01-25 14:54:54 +0100

    Zad 6.1.cpp

--- Zad 6.1.cpp
+++ Zad 6.1.cpp
@@ -3,24 +3,16 @@ using namespace std;
 
 int main() {
     int n;
-
-    // Pobranie wartoœci n od u¿ytkownika
-    cout << "Podaj liczbê n: ";
+    cout << "Podaj liczbe n: ";
     cin >> n;
-
     int liczba = 0;
-
-    // Liczenie liczb spe³niaj¹cych warunki
     for (int i = 1; i <= n; i++) {
         if (i % 5 == 0 && i % 3 != 0) {
             liczba++;
         }
     }
-
-    // Wyœwietlenie wyniku
-    cout << "Liczba liczb naturalnych nie wiêkszych od " << n
-        << ", które s¹ podzielne przez 5, ale nie podzielne przez 3, wynosi: "
+    cout << "Liczba liczb naturalnych nie wiekszych od " << n
+        << ", ktore sa podzielne przez 5, ale nie podzielne przez 3, wynosi: "
         << liczba << endl;
-
     return 0;
 }
